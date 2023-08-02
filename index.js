aadbad5877f3690f24ecc90f4d9dc55aa8550d42
BibekM <bibekmbkb@gmail.com>
2023-08-01 19:03:29 -0700

    In itiated question layout, imported required libraries, initiated init function

---
+++ index.js
@@ -0,0 +1,40 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Gimmi a title:"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Gimmi a title:"
+    },{
+        type: "input",
+        name: "title",
+        message: "Gimmi a title:"
+    },{
+        type: "input",
+        name: "title",
+        message: "Gimmi a title:"
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        
+    });
+}
+
+// Function call to initialize app
+init();
